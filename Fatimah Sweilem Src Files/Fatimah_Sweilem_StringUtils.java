e866e56e813a18b2323a4003ca3edfcce6114f88
Fatimah Sweilem <109782708+SFatimah56@users.noreply.github.com>
2024-04-17 21:23:12 -0700

    Upload Fatimah Sweilem Src Files

---
+++ Fatimah Sweilem Src Files/Fatimah_Sweilem_StringUtils.java
@@ -0,0 +1,21 @@
+package main;
+
+public class StringUtils {
+
+    public static String concatenate(String str1, String str2) {
+        return str1 + str2;
+    }
+
+    public static String reverse(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+
+    public static String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+
+    public static String trim(String str) {
+        return str.trim();
+    }
+}
